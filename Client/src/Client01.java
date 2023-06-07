8bef0326e221b8d205ee36a9881ed07fcc74f944
supunj506 <supunj506@gmail.com>
2023-06-07 18:59:26 +0530

    --- navigate client login form to chat room form

--- Client/src/Client01.java
+++ Client/src/Client01.java
@@ -6,32 +6,46 @@
  * Created by IntelliJ IDEA.
  */
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 
-public class Client01 {
+public class Client01 extends Application {
     public static void main(String[] args) {
-
-        Socket socket;
-        String sendMassage="";
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        try {
-            socket = new Socket("localhost",5000);
-            DataOutputStream outputData = new DataOutputStream(socket.getOutputStream());
-            DataInputStream inputData = new DataInputStream(socket.getInputStream());
-            while(!sendMassage.equals("exit")){
-                sendMassage = bufferedReader.readLine();
-                System.out.println(inputData.readUTF());
-                outputData.writeUTF(sendMassage);
-                System.out.println();
-                outputData.flush();
+        launch(args);
 
 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        Socket socket;
+//        String sendMassage="";
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        try {
+//            socket = new Socket("localhost",5000);
+//            DataOutputStream outputData = new DataOutputStream(socket.getOutputStream());
+//            DataInputStream inputData = new DataInputStream(socket.getInputStream());
+//            while(!sendMassage.equals("exit")){
+//                sendMassage = bufferedReader.readLine();
+//                System.out.println(inputData.readUTF());
+//                outputData.writeUTF(sendMassage);
+//                System.out.println();
+//                outputData.flush();
+//
+//
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/ClientLoginForm.fxml")))));
+        primaryStage.show();
     }
 }
--- Client/src/controller/ClientLoginFormController.java
+++ Client/src/controller/ClientLoginFormController.java
@@ -9,18 +9,43 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ClientLoginFormController {
     public TextField txtUserName;
     public PasswordField txtPassword;
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
+        goToChatRoom();
 
     }
 
     public void btnSigninOnAction(ActionEvent actionEvent) {
+
+    }
+
+    private void goToChatRoom() {
+        try {
+            Stage stage = (Stage) txtUserName.getScene().getWindow();
+            Parent root = FXMLLoader.load(this.getClass().getResource("/view/ChatRoomForm.fxml"));
+
+            stage.setScene(new Scene(root, 404, 669));
+            stage.setOnCloseRequest(event -> {
+                System.exit(0);
+            });
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }

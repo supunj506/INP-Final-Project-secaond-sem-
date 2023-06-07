efb448bb4d86f5b5d1c33891b8234570542a9f3e
supunj506 <supunj506@gmail.com>
2023-06-07 18:40:59 +0530

    --- created client chat room form

---
+++ Client/src/controller/ChatRoomFormController.java
@@ -0,0 +1,30 @@
+/*
+ * @author : @MJ
+ * Date    : 6/7/2023
+ * Time    : 5:37 PM
+ * Project : App1
+ * Created by IntelliJ IDEA.
+ */
+
+package controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ChatRoomFormController {
+    public Label lblUserName;
+    public TextField txtMassage;
+
+    public void openImojeOnAction(MouseEvent mouseEvent) {
+
+    }
+
+    public void openFileOnAction(MouseEvent mouseEvent) {
+
+    }
+
+    public void sendOnAction(MouseEvent mouseEvent) {
+
+    }
+}
